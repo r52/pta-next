1b6b1981bdffbd10f93c797efbafe2801637e2bf
Simon <homer12345@gmail.com>
2021-10-22 11:27:22 -0700

    Don't test for main window since we have none

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,15 +1,15 @@
-const {_electron: electron} = require('playwright');
-const {strict: assert} = require('assert');
+const { _electron: electron } = require('playwright');
+const { strict: assert } = require('assert');
 
 // Playwright has EXPERIMENTAL electron support.
 (async () => {
-  const electronApp = await electron.launch({args: ['.']});
+  const electronApp = await electron.launch({ args: ['.'] });
 
   /**
    * App main window state
    * @type {{isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean}}
    */
-  const windowState = await electronApp.evaluate(({BrowserWindow}) => {
+  const windowState = await electronApp.evaluate(({ BrowserWindow }) => {
     const mainWindow = BrowserWindow.getAllWindows()[0];
 
     const getState = () => ({
@@ -22,7 +22,9 @@ const {strict: assert} = require('assert');
       if (mainWindow.isVisible()) {
         resolve(getState());
       } else
-        mainWindow.once('ready-to-show', () => setTimeout(() => resolve(getState()), 0));
+        mainWindow.once('ready-to-show', () =>
+          setTimeout(() => resolve(getState()), 0),
+        );
     });
   });
 
@@ -37,29 +39,38 @@ const {strict: assert} = require('assert');
    */
   const page = await electronApp.firstWindow();
 
-
   // Check web-page content
-  const element = await page.$('#app', {strict: true});
+  const element = await page.$('#app', { strict: true });
   assert.notStrictEqual(element, null, 'Can\'t find root element');
-  assert.notStrictEqual((await element.innerHTML()).trim(), '', 'Window content is empty');
-
+  assert.notStrictEqual(
+    (await element.innerHTML()).trim(),
+    '',
+    'Window content is empty',
+  );
 
   // Checking the framework.
   // It is assumed that on the main screen there is a `<button>` that changes its contents after clicking.
-  const button = await page.$('button');
-  const originalBtnText = await button.textContent();
+  // const button = await page.$('button');
+  // const originalBtnText = await button.textContent();
 
-  await button.click();
-  const newBtnText = await button.textContent();
+  // await button.click();
+  // const newBtnText = await button.textContent();
 
-  assert.ok(originalBtnText !== newBtnText, 'The button did not change the contents after clicking');
+  // assert.ok(originalBtnText !== newBtnText, 'The button did not change the contents after clicking');
 
   // Check Preload script
   const renderedExposedApi = await page.evaluate(() => globalThis.electron);
   const realVersions = await electronApp.evaluate(() => process.versions);
 
-  assert.notStrictEqual(renderedExposedApi, undefined, 'In renderer `globalThis.electron` is undefined');
-  assert.strictEqual(renderedExposedApi?.versions?.electron, realVersions.electron);
+  assert.notStrictEqual(
+    renderedExposedApi,
+    undefined,
+    'In renderer `globalThis.electron` is undefined',
+  );
+  assert.strictEqual(
+    renderedExposedApi?.versions?.electron,
+    realVersions.electron,
+  );
 
   // Close app
   await electronApp.close();
